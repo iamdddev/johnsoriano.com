ca15ef02f4f6e9f6e4138386e1993d8f81f0320a
John Soriano <developer@johnsoriano.com>
2022-03-13 13:24:16 +0800

    Fix error build

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import Highlighther from "components/highligther";
 import { HeroProps } from ".";
 
 export const Hero: React.FC<HeroProps> = (): React.ReactElement => {
-  const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
-
   return (
     <div>
-      <h1 className="text-3xl  font-semibold text-slate-700 dark:text-slate-200 my-2">I'm John Soriano,</h1>
-      <h2 className="text-2xl  text-slate-700 dark:text-slate-200 my-2">Let's create a better web experience together for humans.</h2>
+      <h1 className="text-3xl  font-semibold text-slate-700 dark:text-slate-200 my-2">I&apos;m John Soriano,</h1>
+      <h2 className="text-2xl  text-slate-700 dark:text-slate-200 my-2">Let&apos;s create a better web experience together for humans.</h2>
     </div>
   );
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,5 @@
-import Button from "components/button";
 import type { NextPage } from "next";
 import Hero from "components/hero";
-import useDarkMode from "hooks/useDarkMode";
 import Layout from "components/layout";
 import Navigation from "components/navigation";
 import Footer from "components/footer";
@@ -26,13 +24,12 @@ const links = [
 ];
 
 const Home: NextPage = () => {
-  const [theme, setTheme] = useDarkMode();
   return (
     <Layout navigation={<Navigation links={links} />} footer={<Footer />}>
       <div>
         <Hero />
-        <div>I'm John Soriano, web developer and integration expert based in Manila, Philippines</div>
-        <div>I've been building things for the web for last 5 years, working with clients accross various industries.</div>
+        <div>I&apos;m John Soriano, web developer and integration expert based in Manila, Philippines</div>
+        <div>I&apos;ve been building things for the web for last 5 years, working with clients accross various industries.</div>
         <div>My Focus is building tools and web integration for existing workflow such as Notion, Airtable, Shopify and some low-code implementation</div>
       </div>
     </Layout>
