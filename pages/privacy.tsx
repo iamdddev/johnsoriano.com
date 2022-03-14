a7ceadebbe560b3e664e61fa68832ba6de133781
John Soriano <developer@johnsoriano.com>
2022-03-14 21:35:31 +0800

    Fix apostrophy error

--- pages/privacy.tsx
+++ pages/privacy.tsx
@@ -124,8 +124,8 @@ const PrivacyPage: NextPage = () => {
         <h3>Usage Data</h3>
         <p>Usage Data is collected automatically when using the Service.</p>
         <p>
-          Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that You
-          visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
+          Usage Data may include information such as Your Device&apos;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that
+          You visit, the time and date of Your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
         </p>
         <p>
           When You access the Service by or through a mobile device, We may collect certain information automatically, including, but not limited to, the type of mobile device You
@@ -220,7 +220,7 @@ const PrivacyPage: NextPage = () => {
           </li>
           <li>
             <p>
-              <b>To contact You:</b> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push
+              <b>To contact You:</b> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application&apos;s push
               notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when
               necessary or reasonable for their implementation.
             </p>
@@ -286,7 +286,7 @@ const PrivacyPage: NextPage = () => {
         </p>
         <h2 className="font-sans text-sm font-extrabold dark:text-slate-50 text-slate-800">Transfer of Your Personal Data</h2>
         <p>
-          Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are
+          Your information, including Personal Data, is processed at the Company&apos;s operating offices and in any other places where the parties involved in the processing are
           located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental
           jurisdiction where the data protection laws may differ than those from Your jurisdiction.
         </p>
@@ -341,20 +341,20 @@ const PrivacyPage: NextPage = () => {
             </p>
           </li>
         </ul>
-        <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Children's Privacy</h1>
+        <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Children&apos;s Privacy</h1>
         <p>
           Our Service does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from anyone under the age of 13. If You are a
           parent or guardian and You are aware that Your child has provided Us with Personal Data, please contact Us. If We become aware that We have collected Personal Data from
           anyone under the age of 13 without verification of parental consent, We take steps to remove that information from Our servers.
         </p>
         <p>
-          If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent's consent
+          If We need to rely on consent as a legal basis for processing Your information and Your country requires consent from a parent, We may require Your parent&apos;s consent
           before We collect and use that information.
         </p>
         <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Links to Other Websites</h1>
         <p>
-          Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We bly
-          advise You to review the Privacy Policy of every site You visit.
+          Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party&apos;s site. We
+          bly advise You to review the Privacy Policy of every site You visit.
         </p>
         <p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
         <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Changes to this Privacy Policy</h1>
--- pages/terms.tsx
+++ pages/terms.tsx
@@ -123,7 +123,7 @@ const TermsPage: NextPage = () => {
         <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Limitation of Liability</h1>
         <p>
           Notwithstanding any damages that You might incur, the entire liability of the Company and any of its suppliers under any provision of this Terms and Your exclusive remedy
-          for all of the foregoing shall be limited to the amount actually paid by You through the Service or 100 USD if You haven't purchased anything through the Service.
+          for all of the foregoing shall be limited to the amount actually paid by You through the Service or 100 USD if You haven&apos;t purchased anything through the Service.
         </p>
         <p>
           To the maximum extent permitted by applicable law, in no event shall the Company or its suppliers be liable for any special, incidental, indirect, or consequential
@@ -134,7 +134,7 @@ const TermsPage: NextPage = () => {
         </p>
         <p>
           Some states do not allow the exclusion of implied warranties or limitation of liability for incidental or consequential damages, which means that some of the above
-          limitations may not apply. In these states, each party's liability will be limited to the greatest extent permitted by law.
+          limitations may not apply. In these states, each party&apos;s liability will be limited to the greatest extent permitted by law.
         </p>
         <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">&quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer</h1>
         <p>
@@ -147,8 +147,8 @@ const TermsPage: NextPage = () => {
           reliability standards or be error free or that any errors or defects can or will be corrected.
         </p>
         <p>
-          Without limiting the foregoing, neither the Company nor any of the company's provider makes any representation or warranty of any kind, express or implied: (i) as to the
-          operation or availability of the Service, or the information, content, and materials or products included thereon; (ii) that the Service will be uninterrupted or
+          Without limiting the foregoing, neither the Company nor any of the company&apos;s provider makes any representation or warranty of any kind, express or implied: (i) as to
+          the operation or availability of the Service, or the information, content, and materials or products included thereon; (ii) that the Service will be uninterrupted or
           error-free; (iii) as to the accuracy, reliability, or currency of any information or content provided through the Service; or (iv) that the Service, its servers, the
           content, or e-mails sent from or on behalf of the Company are free of viruses, scripts, trojan horses, worms, malware, timebombs or other harmful components.
         </p>
@@ -179,8 +179,8 @@ const TermsPage: NextPage = () => {
         </p>
         <h2 className="font-sans text-sm font-extrabold dark:text-slate-50 text-slate-800">Waiver</h2>
         <p>
-          Except as provided herein, the failure to exercise a right or to require performance of an obligation under these Terms shall not effect a party's ability to exercise
-          such right or require such performance at any time thereafter nor shall the waiver of a breach constitute a waiver of any subsequent breach.
+          Except as provided herein, the failure to exercise a right or to require performance of an obligation under these Terms shall not effect a party&apos;s ability to
+          exercise such right or require such performance at any time thereafter nor shall the waiver of a breach constitute a waiver of any subsequent breach.
         </p>
         <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Translation Interpretation</h1>
         <p>
@@ -190,7 +190,7 @@ const TermsPage: NextPage = () => {
         <h1 className="font-sans text-lg font-extrabold dark:text-slate-50 text-slate-800">Changes to These Terms and Conditions</h1>
         <p>
           We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is material We will make reasonable efforts to provide at least
-          30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at Our sole discretion.
+          30 days&apos; notice prior to any new terms taking effect. What constitutes a material change will be determined at Our sole discretion.
         </p>
         <p>
           By continuing to access or use Our Service after those revisions become effective, You agree to be bound by the revised terms. If You do not agree to the new terms, in
