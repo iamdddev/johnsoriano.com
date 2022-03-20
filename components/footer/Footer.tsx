287ac71853370621858029da9052347f20a3e3ed
John Soriano <developer@johnsoriano.com>
2022-03-20 08:49:36 +0800

    Footer update

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,12 +6,6 @@ export const Footer: React.FC<FooterProps> = (): React.ReactElement => {
   return (
     <div className="max-w-2xl mx-auto px-6 md:px-0 py-6 flex justify-between">
       <div className="flex flex-col space-y-4">
-        <Link href="/about">
-          <a className="underline">About</a>
-        </Link>
-        <Link href="/contact">
-          <a className="underline">Contact</a>
-        </Link>
         <Link href="/privacy">
           <a className="underline">Privacy Policy</a>
         </Link>
@@ -19,7 +13,14 @@ export const Footer: React.FC<FooterProps> = (): React.ReactElement => {
           <a className="underline">Terms of service</a>
         </Link>
       </div>
-      <div></div>
+      <div className="flex flex-col space-y-4">
+        <Link href="/about">
+          <a className="underline">About</a>
+        </Link>
+        <Link href="/contact">
+          <a className="underline">Contact</a>
+        </Link>
+      </div>
       <div className="flex flex-col space-y-4">
         <div className="text-slate-500 dark:text-slate-50">All rights reserved &copy; 2022</div>
         <div>Built with NextJS/MDX</div>
